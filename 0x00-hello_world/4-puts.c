62c449422f5aa67c958dbd038e1a8b5acb396974
Esseobong Akpan <esseobongakpan@gmail.com>
2022-09-08 14:14:48 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-
-/**
- * 
+/** 
  * Main - Entry point
  *
  * Description:'Print output'
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
