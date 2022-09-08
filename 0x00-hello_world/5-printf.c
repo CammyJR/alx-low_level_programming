ed8057b5b7c0fadddb1e1cf9ea93dd26b245f191
Esseobong Akpan <esseobongakpan@gmail.com>
2022-09-08 15:03:59 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'Printing with printf'
+ *
+ * Return: always0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
