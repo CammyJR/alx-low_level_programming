ba7b8ee20a08707f8fe1650b970f15d9bb917a0a
Esseobong Akpan <esseobongakpan@gmail.com>
2022-09-12 08:29:14 -0700

    Print single digit munbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - 'Prints single digit numbers'
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int num = '0';
+
+	while(num <= '9')
+	{
+		putchar (num);
+		num++;
+	}
+	putchar ('\n');
+	return (0);
+}
