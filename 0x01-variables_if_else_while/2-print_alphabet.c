044cf8ac02475374015fd4f3b38e793647842dcc
Esseobong Akpan <esseobongakpan@gmail.com>
2022-09-12 05:54:36 -0700

    program to print alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Program to print lowcase alphabets
+ * Return: Always 0
+ */
+
+int main()
+{
+	int c;
+	for (c = 'a' ; c <= 'z' ; c++)
+		putchar(c);
+	return(0);	
+}
