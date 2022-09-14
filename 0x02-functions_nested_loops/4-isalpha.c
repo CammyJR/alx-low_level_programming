83913ac36176f7d15d6a0e0f0aad742c5ebeaae4
Esseobong Akpan <esseobongakpan@gmail.com>
2022-09-13 20:59:04 -0700

    Check if input is an alphabet,case irrespective

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,12 +2,12 @@
 /**
  * _isalpha - checks if input is an alphabet, case irrespective.
  * @c: input parameter
- * Retutn 1 on success, 0 otherwise.
+ * Return: 1 on success, 0 otherwise.
  */
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <=122) || (c >= 64 && c <= 90))
+	if ((c >= 97 && c <= 122) || (c >= 64 && c <= 90))
 		return (1);
-	else 
+	else
 		return (0);
 }
