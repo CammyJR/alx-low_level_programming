83e9d625d52dbad09b0bb6bf17616687edcfcedd
Esseobong Akpan <esseobongakpan@gmail.com>
2022-09-14 21:39:26 -0700

    Print every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,10 +6,11 @@
 void jack_bauer(void)
 {
 	int h = 0;
-	int m = 0;
 
 	while (h < 24)
 	{
+		int m = 0;
+
 		while (m < 60)
 		{
 			_putchar ((h / 10) + '0');
@@ -17,9 +18,9 @@ void jack_bauer(void)
 			_putchar (':');
 			_putchar ((m / 10) + '0');
 			_putchar ((m % 10) + '0');
+			_putchar ('\n');
 			m++;
 		}
-		_putchar ('\n');
 		h++;
 	}
 }
